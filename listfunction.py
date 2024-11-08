731c6e4f034540df9e7eb6cc3905beff4f59440e
Michalyss <michalyss@outlook.com>
2024-11-08 08:46:45 +0000

    Add files via upload

---
+++ listfunction.py
@@ -0,0 +1,10 @@
+animals = ["giraffes", "monkeys", "lions", "turtles", "cats", "dogs",]
+favorite = "My two most loved animals are"
+def fun(x, y,):
+    """
+    :param x: Index of the first animal in the animals list.
+    :param y: Index of the second animal in the animals list.
+    :return: The formatted string combining favorite and the animals at the specified indices.
+    """
+    return print(f"{favorite} {animals[x]} and {animals[y]}.")
+
